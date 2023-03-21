e006c3070213a43cdcf6cee4e91dedf1c295dd1d
gregorioRQ <rafaelgq2000@gmail.com>
2023-03-21 11:24:46 -0300

    tp3

---
+++ tp_3/Ejercicio3.java
@@ -0,0 +1,16 @@
+package tp_3;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char letras[] = {'a','b','c','d','e','f'} ;
+        int i;
+
+        for(i=0; i<letras.length; i++)
+        {
+            System.out.println(letras[i]);
+        }
+	}
+
+}
