feddaa337449c670ff049cb18d50e9eb26fd9522
gregorioRQ <rafaelgq2000@gmail.com>
2023-04-17 17:45:29 -0300

    try catch

--- carritoDeCompras/carritoDeCompras/Main.java
+++ carritoDeCompras/carritoDeCompras/Main.java
@@ -12,72 +12,112 @@ public class Main {
 		String producto = "";
 		double precio = 00.00;
 
-		System.out.println("Bienvenido que desea: ");
-		int num = 0;
-		for (int i = 0; i < nombres.length; i++) {
-			num++;
-			System.out.println("* " + num + " => " + nombres[i] + ": " + precios[i] + " Creditos");
-		}
-		
-		//CON UN SWITCH SELECCIONO EL PRODUCTO 
-		System.out.println("presione un numero para seleccionar su producto");
-		int select = teclado.nextInt();
+		boolean permiso = false;
+		int select = 0;
 		String saborElegido = " ";
-		
-		switch (select) {
-		case 1:
-			producto = nombres[0];
-			precio = precios[0];
-			
-			//SELECCIONA EL SABOR DEL PRODUCTO
-			System.out.println("Que tipo de maní desea:");
-			System.out.println("Presione 0 para salado, 1 tostado, 2 agridulce");			
-			int accion = teclado.nextInt();
-			String[] saboresM = {"salado", "tostado","agridulce"};
-			
-			if(accion == 1) {				
-				saborElegido = saboresM[accion];
-				System.out.println("ha seleccionado mani "+ saboresM[accion]+ " ahora...");
-			}else if(accion == 2) {
-				saborElegido = saboresM[accion];
-				System.out.println("ha seleccionado mani "+ saboresM[accion]+ " ahora...");
-			}else if(accion == 3) {
-				saborElegido = saboresM[accion];
-				System.out.println("ha seleccionado mani "+ saboresM[accion]+ " ahora...");
-			};
-			break;
-		case 2:
-			producto = nombres[1];
-			precio = precios[1];
-			
-			//SELECCIONA EL SABOR DEL PRODUCTO
-			System.out.println("Que sabor de mermelada desea:");
-			System.out.println("Presione 0 para naranja, 1 arandanos, 2 damazco");			
-			int accion2 = teclado.nextInt();
-			String[] saboresMermelada = {"naranja", "arandanos","damazco"};
-			
-			if(accion2 == 1) {				
-				saborElegido = saboresMermelada[accion2];
-				System.out.println("ha seleccionado mermelada de "+ saboresMermelada[accion2]+ " ahora...");
-			}else if(accion2 == 2) {
-				saborElegido = saboresMermelada[accion2];
-				System.out.println("ha seleccionado mermelada de "+ saboresMermelada[accion2]+ " ahora...");
-			}else if(accion2 == 3) {
-				saborElegido = saboresMermelada[accion2];
-				System.out.println("ha seleccionado mermelada de  "+ saboresMermelada[accion2]+ " ahora...");
-			};
-			break;
-		case 3:
-			producto = nombres[2];
-			precio = precios[2];
-			break;
-		}
-			
-		System.out.println("Al hacer una compra superior a 200 creditos recibirá un descuento del 15%, entonces...");
-		System.out.println("cuantas unidades desea lleva?: ");
-		int cantidadP = teclado.nextInt();
 
-		
+		do {
+			try {
+				System.out.println("Bienvenido que desea: ");
+				int num = 0;
+				for (int i = 0; i < nombres.length; i++) {
+					num++;
+					System.out.println("* " + num + " => " + nombres[i] + ": " + precios[i] + " Creditos");
+				}
+
+				System.out.println("presione un numero para seleccionar su producto");
+				select = teclado.nextInt();
+				System.out.println("bien");
+				permiso  = false;
+
+			}catch(java.util.InputMismatchException ex) {
+				System.out.println("No se aceptan caracters sólo enteros");
+				teclado.next();
+				permiso = true;
+			};
+		}while(permiso);
+
+
+		boolean permiso2 = false;
+
+		do {
+			try {
+				int select2 = select;
+
+				switch (select2) {
+				case 1:
+					producto = nombres[0];
+					precio = precios[0];
+
+					//SELECCIONA EL SABOR DEL PRODUCTO
+					System.out.println("Que tipo de maní desea:");
+					System.out.println("Presione 0 para salado, 1 tostado, 2 agridulce");			
+					int accion = teclado.nextInt();
+					String[] saboresM = {"salado", "tostado","agridulce"};
+
+					if(accion == 1) {				
+						saborElegido = saboresM[accion];
+						System.out.println("ha seleccionado mani "+ saboresM[accion]+ " ahora...");
+					}else if(accion == 2) {
+						saborElegido = saboresM[accion];
+						System.out.println("ha seleccionado mani "+ saboresM[accion]+ " ahora...");
+					}else if(accion == 3) {
+						saborElegido = saboresM[accion];
+						System.out.println("ha seleccionado mani "+ saboresM[accion]+ " ahora...");
+					};
+					break;
+				case 2:
+					producto = nombres[1];
+					precio = precios[1];
+
+					//SELECCIONA EL SABOR DEL PRODUCTO
+					System.out.println("Que sabor de mermelada desea:");
+					System.out.println("Presione 0 para naranja, 1 arandanos, 2 damazco");			
+					int accion2 = teclado.nextInt();
+					String[] saboresMermelada = {"naranja", "arandanos","damazco"};
+
+					if(accion2 == 1) {				
+						saborElegido = saboresMermelada[accion2];
+						System.out.println("ha seleccionado mermelada de "+ saboresMermelada[accion2]+ " ahora...");
+					}else if(accion2 == 2) {
+						saborElegido = saboresMermelada[accion2];
+						System.out.println("ha seleccionado mermelada de "+ saboresMermelada[accion2]+ " ahora...");
+					}else if(accion2 == 3) {
+						saborElegido = saboresMermelada[accion2];
+						System.out.println("ha seleccionado mermelada de  "+ saboresMermelada[accion2]+ " ahora...");
+					};
+					break;
+				case 3:
+					producto = nombres[2];
+					precio = precios[2];
+					break;
+				}
+				permiso2 = false;
+			}catch(java.util.InputMismatchException eb) {
+				System.out.println("No se aceptan caracters sólo enteros");
+				teclado.next();
+				permiso2 = true;
+			}
+		}while(permiso2);
+
+		boolean permiso3 = false;
+		int cant = 0;
+		do {
+			try {
+				System.out
+				.println("Al hacer una compra superior a 200 creditos recibirá un descuento del 15%, entonces...");
+				System.out.println("cuantas unidades desea lleva?: ");
+				cant = teclado.nextInt();
+				permiso3 = false;
+			} catch (java.util.InputMismatchException ab) {
+				System.out.println("No se aceptan caracters sólo enteros");
+				teclado.next();
+				permiso3 = true;
+			}
+		} while (permiso3);
+
+		int cantidadP = cant;
+
 		Sabor sa = new Sabor(producto, cantidadP, precio, saborElegido);
 
 		ItemCarrito item = new ItemCarrito(sa.nombreP, sa.cantidadP, sa.precioP,
