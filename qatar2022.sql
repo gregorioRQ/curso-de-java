7b05273bdccb04ecf6f6a2e4a440acbdb4f6fd8a
gregorioRQ <rafaelgq2000@gmail.com>
2023-04-19 12:31:08 -0300

    sql

--- qatar2022.sql
+++ qatar2022.sql
@@ -1,3 +1,6 @@
+
+use qatar2022;
+
 CREATE TABLE departamento(
 NroDepart int(9) not null,
 NomDepart varchar(30) not null,
@@ -12,6 +15,65 @@ ApeEmpleado varchar(20) not null,
 NacionalidadEmpleado varchar(20) not null,
 primary key(DniEmpleado),
 DepEmpleado int(9),
-foreign key(DepEmpleado) references departamento (NroDepart) on delete no action on update no action 
+foreign key(DepEmpleado) references departamento (NroDepart) on delete cascade on update cascade
 )engine=InnoDB;
 
+
+-- CARGA DE DEPARTAMENTOS
+insert into departamento(
+NroDepart,
+NomDepart,
+PresupDepart)values("3", "sistemas","33.50");
+
+insert into departamento(
+NroDepart,
+NomDepart,
+PresupDepart)values("2","compras","22.20");
+
+insert into departamento(
+NroDepart,
+NomDepart,
+PresupDepart)values("1", "logistica","44.10");
+
+
+-- CARGA DE EMPLEADOS
+insert into empleados(
+DniEmpleado,
+NomEmpleado,
+ApeEmpleado,
+NacionalidadEmpleado,
+DepEmpleado) values(
+'202041', 'Julio', 'Ortega', 'Cubano', '3'
+);
+
+insert into empleados(
+DniEmpleado,
+NomEmpleado,
+ApeEmpleado,
+NacionalidadEmpleado,
+DepEmpleado)values("158923","Alberto","Quintero","Chile","2"
+);
+
+insert into empleados(
+DniEmpleado,
+NomEmpleado,
+ApeEmpleado,
+NacionalidadEmpleado,
+DepEmpleado) values(
+'432314', 'Damian', 'Sosa', 'Ven', '1'
+);
+
+
+select * from empleados ;
+-- ACTUALIZACION DE NACIONALIDAD
+update empleados set NacionalidadEmpleado = 'Español' where DniEmpleado= 432314;
+select * from empleados;
+
+-- DEPARTAMENTO DE LOGISTICA
+select * from empleados where DepEmpleado=1;
+
+-- DEPARTAMENTOS ORDENADOS ALFABETICAMENTE EN ORDEN DESCENDENTE
+select * from departamento order by NomDepart desc;
+
+-- ELIMINAR DEPARTAMENTO
+delete from departamento where NroDepart=3;
